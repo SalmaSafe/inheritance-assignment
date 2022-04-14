c657ca5be88f71bb6238e1effdc3fbe187a93a60
SalmaSafe <salmaasulee@gmail.com>
2022-04-14 09:40:26 +0300

    first commit

--- src/main.kt
+++ src/main.kt
@@ -1,63 +1,71 @@
 fun main(){
-     var car=Car("Subsidiary","mercedes-Benz GLE 350","Black", 5)
-      car.carry(7)
-    car.identity()
-    car.calculateParkingFee(3)
-
-    var bus=Bus("Toyo,a","toyota mazda","Grey",30)
-    bus.maxTripFare(80.50)
-    bus.calculateParkingFee(2)
-
+    var sa= current(1036900,"mama",3000)
+ deposit(200.50)
+    fruits("Banana")
+    var p=prod()
 
 }
-//-carry(people: Int) : Prints out “Carrying $peoplepassengers” if the
-//number of people is within its capacity else it prints out “Over capacity
-//by $x people” where x is the number of people exceeding its capacity
+
+//1.Create a classCurrentAccount with the following attributes:account
+//number, account name, balance. It has the following functions:
+//a.deposit(amount: Double) - Increments the balance bythe
+//amount deposited
+//b.withdraw(amount: Double) - Decrements the balanceby the
+//amount withdrawn
+//c.details() - Prints out the account number and balanceand
+//name in this format: “Account number x with balance y is
+//operated by z
+open class current(var accNo : Int, var name:String,var balan:Int)
+
+fun deposit(amount:Double){
+    balan+=amount
+    println(balan)
+
+
+
+    }
+
+fun withdraw(amount:Double){
+    balance-=amount
+    println(amount)
+}
+fun details(x: Int,y: Int,z: String){
+    println("Account number $x with balance $y is operated by $z")
+
+}
+//Create another classSavingsAccount. It has the samefunctions and
+//attributes as the current account except for one attribute,
+//withdrawals: Int. Withdrawals is a counter variablethat is used to
+//keep track of how many withdrawals have been made from the account
+//in a year. The only other difference is that the savings account
+//withdraw() method first checks if the number of withdrawalsis less
+//than 4 for it to allow one to withdraw money from the account. It also
+//increments the withdrawals attribute after a successful withdrawal
+//(5poi
+
+class savingAccount(var accNo : Int, var name:String,var balan:Int, var withdrawals:Int ):current(accNo,name,balan){
+    fun with(withdraw:Int){
+        if(withdraw < 4 ){
+            println(withdraw)
+}
+//A product is represented by a data class with these attributes: name,
+//weight, price, category. Category can either be groceries, hygiene or
+//other. Write a function that takes in a product and uses a when
+//statement to assign each product to a list based on its category
 //(3 points)
-//-identity() : Prints out the color, make and modelin the following
-//format e.g:  “I am a white subaru legacy”(1 point)
-//-calculateParkingFees(hours: Int) : Calculates andreturns the
-//parking fees by multiplying the hours by 20(1 point)
-open class Car(var make:String, var model:String,var color:String,var capacity:Int){
-    fun carry(people:Int){
-        var excess=people - capacity
-        if (people<=5){
-            println("carrying $people passengers")
-        }
-        else
-            (println("Overcapacity by $excess people"))
+data class prod(var name: String,var weight:Int,var price:Double,var category:String){
 
-    }
-    fun identity(){
-        println("I have a $color $make $model")
-    }
-     open fun calculateParkingFee(hours:Int):Int{
-          var time= hours * 20
-        println(time)
-         return time
-    }
 }
-//Create a class Bus with the same attributes and functions as the Car class.
-//In addition, it has another method called maxTripFare(fare: Double) that
-//calculates and returns the maximum amount of fare that can be collected per
-//trip.(2 points)
-//The bus’calculateParkingFees method returns the productof hours and
-//the capacity of the bus
-class Bus(make:String, model:String,color:String,capacity:Int):Car(make,model,color,capacity){
-    fun maxTripFare(fare:Double): Double{
-        var tripFare= capacity * fare
-        println(tripFare)
-        return tripFare
+//4.Write a function that given a string returns a string composed of only
+//the characters in even indices. For example given “banana” it will return
+//“bnn
+fun fruits (word:String):String{
+    var name=""
+    for (letter in word){
+        if (word.indexOf(letter)%2==0){
+            name+=letter
+            println(name)
+        }
     }
-
-    override fun calculateParkingFee(hours: Int):Int{
-        var product= hours* capacity
-        println(product)
-
-        return product
-
-    }
-
-
-
+    return name
 }
